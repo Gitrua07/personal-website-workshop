01a0e5306458b26ecb5434677451121cd1d35f47
Gaia Iturralde <Iturralde.gaia@gmail.com>
2025-11-01 14:53:24 -0700

    Initial commit of React app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {HashRouter as Router, Routes, Route } from "react-router-dom";
+import NavigationBar from "./components/NavigationBar"
+import Home from "./components/Home"
+import Projects from "./components/Projects"
+import About from "./components/About"
+import Experience from "./components/Experience"
+import './App.css'; //Imports css file
 
-function App() {
+//HashRouter provides you a separate link per page
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavigationBar />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/experience" element={<Experience />}/>
+        <Route path="/projects" element={<Projects />}/>
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
